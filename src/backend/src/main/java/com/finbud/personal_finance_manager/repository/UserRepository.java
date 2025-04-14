5be5c9ebb97420ea1f279c39755f60cca612cfe9
dre <drewoodward@gmail.com>
2025-04-14 17:47:35 -0400

    sql integration

---
+++ src/backend/src/main/java/com/finbud/personal_finance_manager/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.finbud.personal_finance_manager.repository;
+
+import com.finbud.personal_finance_manager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
+
