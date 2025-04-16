25c87f6c629d1b4d5daa411d53ee07c603239f3c
dre <drewoodward@gmail.com>
2025-04-16 13:47:44 -0400

    REST endpoints

--- src/backend/src/main/java/com/finbud/personal_finance_manager/HelloController.java
+++
@@ -1,8 +0,0 @@
-package com.finbud.personal_finance_manager;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-public class HelloController {
-    @RequestMapping("/hello")
-    public String hello() { return "Hello World!"; }
-}
